abb73e6ab97573c88efd39bf1c4c5c372dd9cbe1
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-02 14:12:29 -0700

    add drei, orbital controls, stats and perf, polyhedron, update readme with journal

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-/* eslint-disable react/no-unknown-property */
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import Box from "./Box";
+import Polyhedron from "./Polyhedron";
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from 'three'
+// import { Stats } from "@react-three/drei";
+import { Perf } from 'r3f-perf'
 
-function App() {
+
+export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ]
+
   return (
     <>
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <Box position={[-0.75, 0, 0]} name="A" />
-        <Box position={[0.75, 0, 0]} name="B" />
-        <OrbitControls />
+      <Canvas camera={{ position: [0, 0, 3] }}>
+        <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+        <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+        <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+        <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+        <OrbitControls 
+        minAzimuthAngle={-Math.PI / 4}
+        maxAzimuthAngle={Math.PI / 4}
+        minPolarAngle={Math.PI / 6}
+        maxPolarAngle={Math.PI - Math.PI / 6}
+
+        />
+        <Perf position="top-left"/>
       </Canvas>
     </>
   );
 }
-
-export default App;
