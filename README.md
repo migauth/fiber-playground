9d07923ae9e71f5989beb99ab6f7ac004672afbd
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-03 11:27:49 -0700

    add and experiment with leva

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Oct 2
 Oct 3
 - `<PointerLockControls />`: creates a first person view
 - `<axisHelper />`: adds colorful lines at x y z
+- `<gridHelper />`: adds a grid
+- Liva: a GUI component to interact with scene - import { useControls } from 'leva'
--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Polyhedron from "./Polyhedron";
 import { OrbitControls } from "@react-three/drei";
-import * as THREE from 'three'
+import * as THREE from "three";
 import { Stats } from "@react-three/drei";
 // import { Perf } from 'r3f-perf'
-
+import { useControls } from "leva";
 
 export default function App() {
   const polyhedron = [
     new THREE.BoxGeometry(),
     new THREE.SphereGeometry(0.785398),
     new THREE.DodecahedronGeometry(0.785398),
-  ]
+  ];
+
+const color = useControls({
+  value: 'green',
+})
 
   return (
     <>
       <Canvas camera={{ position: [0, 0, 3] }}>
+        <color attach="background" args={[color.value]} />
         <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
         <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
         <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
@@ -25,6 +30,7 @@ export default function App() {
         <OrbitControls />
         <Stats />
         <axesHelper args={[5]} />
+        <gridHelper />
       </Canvas>
     </>
   );
