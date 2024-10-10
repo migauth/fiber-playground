457a0a871fc3e8b6dfd19c377c0abd55e471e380
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-10 11:57:33 -0700

    annotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,67 @@
-/* eslint-disable react/no-unknown-property */
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { Stats, OrbitControls, Environment, useGLTF } from "@react-three/drei";
-import { useControls } from "leva";
+import { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  useGLTF,
+  OrbitControls,
+  Environment,
+  Stats,
+  Html,
+} from '@react-three/drei'
+import { useControls } from 'leva'
+import Models from './models.json'
 
-const Models = [{ title: "Wrench", url: "/img/wrench.glb" }];
-
-// eslint-disable-next-line react/prop-types
 function Model({ url }) {
-  const { scene } = useGLTF(url);
-  return <primitive object={scene} />;
+  const { scene } = useGLTF(url)
+  const [cache, setCache] = useState({})
+
+  if (!cache[url]) {
+    const annotations = []
+
+    scene.traverse((o) => {
+      if (o.userData.prop) {
+        annotations.push(
+          <Html
+            key={o.uuid}
+            position={[o.position.x, o.position.y, o.position.z]}
+            distanceFactor={0.25}
+          >
+            <div className="annotation">{o.userData.prop}</div>
+          </Html>
+        )
+      }
+    })
+
+    console.log('Caching JSX for url ' + url)
+    setCache({
+      ...cache,
+      [url]: <primitive object={scene}>{annotations}</primitive>,
+    })
+  }
+  return cache[url]
 }
 
 export default function App() {
-  const { title } = useControls({
-    title: {
-      options: Models.map(({ title }) => title),
+  const { model } = useControls({
+    model: {
+      value: 'drill',
+      options: Object.keys(Models),
     },
-  });
+  })
 
   return (
     <>
       <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
         <Environment files="/img/workshop_1k.hdr" background />
         <group>
-          <Model url={Models[Models.findIndex((m) => m.title === title)].url} />
+          <Model url={Models[model]} />
         </group>
-
         <OrbitControls autoRotate />
         <Stats />
       </Canvas>
-      <span id="info">The {title} is selected.</span>
+      <span id="info">
+        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
+      </span>
     </>
-  );
+  )
 }
