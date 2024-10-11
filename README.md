efebcb9b445d32273319f6b85c87b8878e90c81d
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-11 09:25:59 -0700

    GLTFJSX

--- README.md
+++ README.md
@@ -43,3 +43,4 @@ Oct 9
 Oct 10
 - useGLTF - create models in blender and export them as glb files. Models are loaded dynamically using the useGLTF hook.
 - annotations - Drei’s Html component allows you to place HTML elements within the 3D scene and have them interact seamlessly. You can add these annotations directly in Blender by embedding metadata (like userData) into the model. For each relevant part of the model, you can add a prop property to the userData, which stores the annotation text.
+- GLTFJSX - GLTFJSX is a command line tool that will read through a glTF model, and extract its components into a JSX equivalent.
--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,17 @@
 import "./App.css";
-import { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import {
-  useGLTF,
-  OrbitControls,
-  Environment,
-  Stats,
-  Html,
-} from '@react-three/drei'
-import { useControls } from 'leva'
-import Models from './models.json'
-
-function Model({ url }) {
-  const { scene } = useGLTF(url)
-  const [cache, setCache] = useState({})
-
-  if (!cache[url]) {
-    const annotations = []
-
-    scene.traverse((o) => {
-      if (o.userData.prop) {
-        annotations.push(
-          <Html
-            key={o.uuid}
-            position={[o.position.x, o.position.y, o.position.z]}
-            distanceFactor={0.25}
-          >
-            <div className="annotation">{o.userData.prop}</div>
-          </Html>
-        )
-      }
-    })
-
-    console.log('Caching JSX for url ' + url)
-    setCache({
-      ...cache,
-      [url]: <primitive object={scene}>{annotations}</primitive>,
-    })
-  }
-  return cache[url]
-}
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment, ContactShadows } from "@react-three/drei";
+import Model from "./Shoe";
 
 export default function App() {
-  const { model } = useControls({
-    model: {
-      value: 'drill',
-      options: Object.keys(Models),
-    },
-  })
-
   return (
     <>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-        <Environment files="/img/workshop_1k.hdr" background />
-        <group>
-          <Model url={Models[model]} />
-        </group>
+      <Canvas camera={{ position: [0, 0, -0.2] }}>
+        <Environment preset="forest" />
+        <Model />
+        <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
         <OrbitControls autoRotate />
-        <Stats />
       </Canvas>
-      <span id="info">
-        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
-      </span>
     </>
-  )
+  );
 }
